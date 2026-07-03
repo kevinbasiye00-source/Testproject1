6fdd4ace736e4165ae0ec9d4ad8491c8c6c02876
“Kevin <kevinbasiye00@gmail.com>
2026-07-03 19:59:39 +0300

    Remove merge conflict markers

--- index.py
+++ index.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print("Hello World")print(Good evening)
-=======
+print("Hello World")print("Good evening")
 print("Hello World")
 print("Hello DS-PT16")
->>>>>>> 139b2a4b94d1115b382b6ce6f56790d3d9099bd1
